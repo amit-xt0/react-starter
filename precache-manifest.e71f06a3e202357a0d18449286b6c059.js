dbb7ef6dc5d55241abc5803e664aed86ac13c1c0
Amit <amit.xt0@gmail.com>
2020-06-04 17:31:39 +0530

    Updates

---
+++ precache-manifest.e71f06a3e202357a0d18449286b6c059.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d2e32abf8dde3a9fd3b8e9730f3208",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af62ac20aca7ac82184f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bbece9c4159f40eb3ae7",
+    "url": "/static/js/2.624bc9aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.624bc9aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af62ac20aca7ac82184f",
+    "url": "/static/js/main.b34f135f.chunk.js"
+  },
+  {
+    "revision": "b396d0fc20b42dc2015f",
+    "url": "/static/js/runtime-main.36a1111f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
